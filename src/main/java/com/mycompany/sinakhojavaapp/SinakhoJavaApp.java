9a5031dae2925542328fe8140be756fa711dfd8a
SinakhoM <ST10531540@myemeris.edu.za>
2026-04-22 16:28:35 +0200

    Update SinakhoJavaApp.java

--- src/main/java/com/mycompany/sinakhojavaapp/SinakhoJavaApp.java
+++ src/main/java/com/mycompany/sinakhojavaapp/SinakhoJavaApp.java
@@ -4,13 +4,37 @@
 
 package com.mycompany.sinakhojavaapp;
 
+import java.util.Scanner;
+
 /**
  *
  * @author cash
  */
 public class SinakhoJavaApp {
-
+// Setting up the Scanner and Login objects to handle input and logic
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+   // 1. Setup the tools
+    Scanner input = new Scanner(System.in);
+    Login loginSystem = new Login();
+
+    // 2. The Greeting
+    System.out.println("Welcome to Sinakho App!");
+    
+    // 3. Collect Data
+    System.out.print("Please enter your First Name: ");
+    String fName = input.nextLine();
+    
+    System.out.print("Please enter your Last Name: ");
+    String lName = input.nextLine();
+
+    System.out.println("Enter a username (Must have '_' and be max 5 chars): ");
+    String user = input.nextLine();
+
+    // 4. Use your Login rules!
+    if (loginSystem.checkUserName(user)) {
+        System.out.println("Username successfully captured");
+    } else {
+        System.out.println("Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length.");
+    }     
     }
 }
